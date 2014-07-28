f7d79e879f17a868e4ff99c4207777af26307c1f
Romain Gauthier <romain.gauthier@monkeypatch.me>
2014-07-28 18:06:38 +0200

    Refactor encoder and decoder into objects

--- spec/TNetBinSpec.js
+++ spec/TNetBinSpec.js
@@ -1,128 +1,151 @@
 describe("tnetbin.js", function() {
 
-    describe('encodes', function() {
+  describe("Encoder", function() {
+    var encoder = new tnetbin.Encoder();
 
-        it("null", function() {
-            expect(tnetbin.encode(null)).toBe('0:~');
-        });
+    describe('#encode', function() {
 
-        it("booleans", function() {
-            expect(tnetbin.encode(true)).toBe('4:true!');
-            expect(tnetbin.encode(false)).toBe('5:false!');
-        });
+      it("null", function() {
+        expect(encoder.encode(null)).toBe('0:~');
+      });
 
-        it("integers", function() {
-            expect(tnetbin.encode(123)).toBe('3:123#');
-        });
+      it("booleans", function() {
+        expect(encoder.encode(true)).toBe('4:true!');
+        expect(encoder.encode(false)).toBe('5:false!');
+      });
 
-        it("floats", function() {
-            expect(tnetbin.encode(3.141592653589793)).toBe('17:3.141592653589793^');
-        });
+      it("integers", function() {
+        expect(encoder.encode(123)).toBe('3:123#');
+      });
 
-        it("strings", function() {
-            expect(tnetbin.encode('Back to the Future')).toBe('18:Back to the Future,');
-        });
+      it("floats", function() {
+        expect(encoder.encode(3.141592653589793)).toBe('17:3.141592653589793^');
+      });
 
-        it("array buffers", function() {
-            var buffer = new ArrayBuffer(8);
-            var s = String.fromCharCode.apply(null, new Uint8Array(buffer));
-            var result = '8:' + s + ',';
-            expect(tnetbin.encode(buffer)).toBe(result)
-        });
+      it("strings", function() {
+        expect(encoder.encode('Back to the Future')).toBe('18:Back to the Future,');
+      });
 
-        it("lists", function() {
-            var array = ["hello", 12345, 3.14, false];
-            var result = "31:5:hello,5:12345#4:3.14^5:false!]";
-            expect(tnetbin.encode(array)).toBe(result);
-        });
+      it("array buffers", function() {
+        var buffer = new ArrayBuffer(8);
+        var s = String.fromCharCode.apply(null, new Uint8Array(buffer));
+        var result = '8:' + s + ',';
+        expect(encoder.encode(buffer)).toBe(result)
+      });
 
-        it("dicts", function() {
-            var object = {a: "hello", b: 12345, c: 3.14, d: false};
-            var result = "47:1:a,5:hello,1:b,5:12345#1:c,4:3.14^1:d,5:false!}";
-            expect(tnetbin.encode(object)).toBe(result);
-        });
+      it("lists", function() {
+        var array = ["hello", 12345, 3.14, false];
+        var result = "31:5:hello,5:12345#4:3.14^5:false!]";
+        expect(encoder.encode(array)).toBe(result);
+      });
+
+      it("dicts", function() {
+        var object = {a: "hello", b: 12345, c: 3.14, d: false};
+        var result = "47:1:a,5:hello,1:b,5:12345#1:c,4:3.14^1:d,5:false!}";
+        expect(encoder.encode(object)).toBe(result);
+      });
 
     });
 
-    describe("decodes", function() {
-        it("null", function() {
-            expect(tnetbin.decode('0:~').value).toBe(null);
-        });
+  });
 
-        it("booleans", function() {
-            expect(tnetbin.decode('4:true!').value).toBe(true);
-            expect(tnetbin.decode('5:false!').value).toBe(false);
-        });
+  describe("Decoder", function() {
 
-        it("integers", function() {
-            expect(tnetbin.decode('11:12345678901#').value).toBe(12345678901);
-        });
+    var decoder = new tnetbin.Decoder();
 
-        it("floats", function() {
-            expect(tnetbin.decode('17:3.141592653589793^').value).toBe(3.141592653589793);
-        });
+    describe("#decode", function() {
+      it("null", function() {
+        expect(decoder.decode('0:~').value).toBe(null);
+      });
 
-        it("strings", function() {
-            expect(tnetbin.decode('18:Back to the Future,').value).toBe('Back to the Future');
-        });
+      it("booleans", function() {
+        expect(decoder.decode('4:true!').value).toBe(true);
+        expect(decoder.decode('5:false!').value).toBe(false);
+      });
 
-        it("array buffers", function() {
-            var buffer = new ArrayBuffer(22);
-            var view   = new Uint8Array(buffer);
-            for (var i=0; i < 22; i++)
-                view[i] = '18:Back to the Future,'.charCodeAt(i);
-            expect(tnetbin.decode(buffer).value).toBe('Back to the Future');
-        });
+      it("integers", function() {
+        expect(decoder.decode('11:12345678901#').value).toBe(12345678901);
+      });
 
-        it("lists", function() {
-            var tnet = "31:5:hello,5:12345#4:3.14^5:false!]";
-            var list = ["hello", 12345, 3.14, false];
-            expect(tnetbin.decode(tnet).value).toEqual(list);
-        });
+      it("floats", function() {
+        expect(decoder.decode('17:3.141592653589793^').value).toBe(3.141592653589793);
+      });
 
-        it("dicts", function() {
-            var tnet = "47:1:a,5:hello,1:b,5:12345#1:c,4:3.14^1:d,5:false!}";
-            var dict = {a: "hello", b: 12345, c: 3.14, d: false};
-            expect(tnetbin.decode(tnet).value).toEqual(dict);
-        });
+      it("strings", function() {
+        expect(decoder.decode('18:Back to the Future,').value).toBe('Back to the Future');
+      });
 
-        it("one element at a time", function() {
-            var tnet = "12:1:1#1:2#1:3#]1:4#", result;
+      it("array buffers", function() {
+        var buffer = new ArrayBuffer(22);
+        var view   = new Uint8Array(buffer);
+        for (var i=0; i < 22; i++)
+          view[i] = '18:Back to the Future,'.charCodeAt(i);
+        expect(decoder.decode(buffer).value).toBe('Back to the Future');
+      });
 
-            result = tnetbin.decode(tnet);
-            expect(result.value).toEqual([1, 2, 3]);
-            expect(result.remain).toEqual('1:4#');
+      it("lists", function() {
+        var tnet = "31:5:hello,5:12345#4:3.14^5:false!]";
+        var list = ["hello", 12345, 3.14, false];
+        expect(decoder.decode(tnet).value).toEqual(list);
+      });
 
-            result = tnetbin.decode(result.remain);
-            expect(result.value).toEqual(4);
-            expect(result.remain).toEqual('');
-        });
+      it("dicts", function() {
+        var tnet = "47:1:a,5:hello,1:b,5:12345#1:c,4:3.14^1:d,5:false!}";
+        var dict = {a: "hello", b: 12345, c: 3.14, d: false};
+        expect(decoder.decode(tnet).value).toEqual(dict);
+      });
+
+      it("one element at a time", function() {
+        var tnet = "12:1:1#1:2#1:3#]1:4#", result;
+
+        result = decoder.decode(tnet);
+        expect(result.value).toEqual([1, 2, 3]);
+        expect(result.remain).toEqual('1:4#');
+
+        result = decoder.decode(result.remain);
+        expect(result.value).toEqual(4);
+        expect(result.remain).toEqual('');
+      });
     });
+
+    describe("#isAString", function() {
+
+      it("should be false for ArrayBuffers of odd length", function() {
+        var buffer = new ArrayBuffer(3);
+        expect(tnetbin.isAString(buffer)).toBe(false);
+      });
+
+    });
+
+  });
+
 });
 
 describe("edge cases/bugs", function() {
-    it("should decode empty lists", function() {
-        expect(tnetbin.decode('0:]').value).toEqual([]);
-    });
+  var encoder = new tnetbin.Encoder();
+  var decoder = new tnetbin.Decoder();
 
-    it("should decode empty dicts", function() {
-        expect(tnetbin.decode('0:}').value).toEqual({});
-    });
+  it("should decode empty lists", function() {
+    expect(decoder.decode('0:]').value).toEqual([]);
+  });
 
-    it("should decode ArrayBuffers of odd length", function() {
-        var buffer = new ArrayBuffer(3);
-        var view = new Uint8Array(buffer);
-        for (var i=0; i < 3; i++)
-            view[i] = '0:}'.charCodeAt(i);
-        expect(tnetbin.decode(buffer).value).toEqual({});
-    });
+  it("should decode empty dicts", function() {
+    expect(decoder.decode('0:}').value).toEqual({});
+  });
 
-    it("should encode large ArrayBuffers", function() {
-        var buffer = new ArrayBuffer(512 * 1024);
-        var view = new Uint8Array(buffer);
-        for (var i = 0; i < buffer.byteLength; i++)
-            view[i] = 97;
-        expect(tnetbin.encode(buffer).length).toEqual(512 * 1024 + 8);
-    });
+  it("should decode ArrayBuffers of odd length", function() {
+    var buffer = new ArrayBuffer(3);
+    var view = new Uint8Array(buffer);
+    for (var i=0; i < 3; i++)
+      view[i] = '0:}'.charCodeAt(i);
+    expect(decoder.decode(buffer).value).toEqual({});
+  });
 
+  it("should encode large ArrayBuffers", function() {
+    var buffer = new ArrayBuffer(512 * 1024);
+    var view = new Uint8Array(buffer);
+    for (var i = 0; i < buffer.byteLength; i++)
+      view[i] = 97;
+    expect(encoder.encode(buffer).length).toEqual(512 * 1024 + 8);
+  });
 });
--- tnetbin.js
+++ tnetbin.js
@@ -1,14 +1,10 @@
-// TNetBin.js 0.2.1
+var tnetbin = (function() {
+  function Encoder(options) {
+    this.options = options || {};
+  }
 
-// (c) 2012-2013 Romain Gauthier.
-// TNetBin may be freely distributed under the MIT license.
-// For all details and documentation:
-// https://github.com/tOkeshu/tnetbin.js
-
-(function(globalScope) {
-
-  var tnetbin = {
-    encode: function(obj) {
+  Encoder.prototype = {
+    encode: function(obj, options) {
       switch (obj) {
       case null:
         return '0:~';
@@ -39,14 +35,14 @@
           s = largeArrayToString(obj);
           tag = ',';
         } else if (obj instanceof Array) { // List
-          s = obj.map(tnetbin.encode).join('');
+          s = obj.map(this.encode).join('');
           tag = ']';
         } else { // Object
           var attrs = [];
           for (var attr in obj) {
             if (obj.hasOwnProperty(attr)) {
-              attrs.push(tnetbin.encode(attr),
-                         tnetbin.encode(obj[attr]));
+              attrs.push(this.encode(attr),
+                         this.encode(obj[attr]));
             }
           }
           s = attrs.join('');
@@ -55,29 +51,12 @@
       }
 
       return s.length + ':' + s + tag;
-    },
-
-    decode: function(data) {
-      var result;
-      data = this.toArrayBuffer(data);
-      result = _decode(data, 0);
-
-      return {value: result.value, remain: remain(data, result.cursor)};
-    },
-
-    toArrayBuffer: function(data) {
-      if (data instanceof ArrayBuffer)
-        return new Uint8Array(data);
-
-      var len  = data.length;
-      var view = new Uint8Array(len);
-      // Transform the string to an array buffer
-      for (var cursor = 0; cursor < len; cursor++)
-        view[cursor] = data.charCodeAt(cursor);
-
-      return view;
     }
   };
+
+  function Decoder(options) {
+    this.options = options || {};
+  }
 
   var COLON   = 58;
   var ZERO    = 48;
@@ -88,6 +67,152 @@
   var STRING  = 44;
   var LIST    = 93;
   var DICT    = 125;
+
+  Decoder.prototype = {
+    decode: function(data) {
+      var result;
+      data = toArrayBuffer(data);
+      result = this._decode(data, 0);
+
+      return {value: result.value, remain: this.remain(data, result.cursor)};
+    },
+
+    _decode: function(data, cursor) {
+      return this._decodeSize(data, cursor, this._decodePayload.bind(this));
+    },
+
+    _decodeSize: function(data, cursor, callback) {
+      for (var size=0; data[cursor] !== COLON; cursor++) {
+        size = size*10 + (data[cursor] - ZERO);
+      }
+
+      return callback(data, cursor + 1, size);
+    },
+
+    _decodePayload: function(data, cursor, size) {
+      var tag = data[cursor + size];
+      switch (tag) {
+      case NULL:
+        return this._decodeNull(data, cursor, size);
+      case BOOLEAN:
+        return this._decodeBoolean(data, cursor, size);
+      case INTEGER:
+        return this._decodeInteger(data, cursor, size);
+      case FLOAT:
+        return this._decodeFloat(data, cursor, size);
+      case STRING:
+        return this._decodeString(data, cursor, size);
+      case LIST:
+        return this._decodeList(data, cursor, size);
+      case DICT:
+        return this._decodeDict(data, cursor, size);
+      default:
+        return {value: undefined, cursor: cursor + size + 1};
+      }
+    },
+
+    remain: function(data, cursor) {
+      var d = data.subarray(cursor);
+      return largeArrayToString(d);
+    },
+
+    _decodeNull: function(data, cursor) {
+      return {value: null, cursor: cursor + 1};
+    },
+
+    _decodeBoolean: function(data, cursor, size) {
+      return {value: (size === 4) ? true : false, cursor: cursor + size + 1};
+    },
+
+    _decodeInteger: function(data, cursor, size) {
+      var end = cursor + size;
+      for (var value = 0; cursor < end; cursor++)
+        value = value*10 + (data[cursor] - ZERO);
+
+      return {value: value, cursor: cursor + 1};
+    },
+
+    _decodeFloat: function(data, cursor, size) {
+      var value, exp, decimal;
+      var end = cursor + size;
+
+      for(value = 0; data[cursor] !== 46; cursor++)
+        value = value*10 + (data[cursor] - ZERO);
+
+      cursor++;
+      for (decimal = 0, exp=1; cursor < end; cursor++, exp*=10) {
+        decimal = decimal*10 + (data[cursor] - ZERO);
+      }
+
+      return {value: value + (decimal/exp), cursor: cursor + 1};
+    },
+
+    _decodeString: function(data, cursor, size) {
+      var s;
+      data = data.subarray(cursor, cursor + size);
+
+      if (true) //isAString(d))
+        return {value: largeArrayToString(data), cursor: cursor + size + 1};
+      else
+        return {value: data, cursor: cursor + size + 1};
+    },
+
+    _decodeList: function(data, cursor, size) {
+      if (size === 0)
+        return {value: [], cursor: cursor + 1};
+
+      var list = [];
+      var end  = cursor + size;
+      var result;
+
+      do {
+        result = this._decode(data, cursor);
+        list.push(result.value);
+        cursor = result.cursor;
+      } while (cursor < end);
+
+      return {value: list, cursor: result.cursor + 1};
+    },
+
+    _decodeDict: function(data, cursor, size) {
+      if (size === 0)
+        return {value: {}, cursor: cursor + 1};
+
+      var dict = {};
+
+      var result = this._decodeList(data, cursor, size);
+      var items  = result.value;
+      var len    = items.length;
+      for (var i = 0; i < len; i+=2)
+        dict[items[i]] = items[i + 1];
+
+      return {value: dict, cursor: result.cursor};
+    }
+  };
+
+  function isAString(data) {
+    if (data.byteLength % 2 !== 0)
+      return false;
+
+    data = new Uint16Array(data.buffer);
+    for (var i=0; i < data.byteLength; i++)
+      if (0x000 <= data[i] && data[i] <= 0x00)
+        return false
+    return true;
+  }
+
+  function toArrayBuffer(data) {
+    if (data instanceof ArrayBuffer)
+      return new Uint8Array(data);
+
+    var len  = data.length;
+    var view = new Uint8Array(len);
+    // Transform the string to an array buffer
+    for (var cursor = 0; cursor < len; cursor++)
+      view[cursor] = data.charCodeAt(cursor);
+
+    return view;
+  }
 
   function splitArrayBuffer(data) {
     var len = data.byteLength;
@@ -105,114 +230,10 @@
     return s;
   }
 
-  function _decode(data, cursor) {
-    return _decodeSize(data, cursor, _decodePayload);
+  return {
+    Encoder: Encoder,
+    Decoder: Decoder,
+    isAString: isAString
   }
-
-  function _decodeSize(data, cursor, callback) {
-    for (var size=0; data[cursor] !== COLON; cursor++) {
-      size = size*10 + (data[cursor] - ZERO);
-    }
-
-    return callback(data, cursor + 1, size);
-  }
-
-  function _decodePayload(data, cursor, size) {
-    var tag = data[cursor + size];
-    switch (tag) {
-    case NULL:
-      return _decodeNull(data, cursor, size);
-    case BOOLEAN:
-      return _decodeBoolean(data, cursor, size);
-    case INTEGER:
-      return _decodeInteger(data, cursor, size);
-    case FLOAT:
-      return _decodeFloat(data, cursor, size);
-    case STRING:
-      return _decodeString(data, cursor, size);
-    case LIST:
-      return _decodeList(data, cursor, size);
-    case DICT:
-      return _decodeDict(data, cursor, size);
-    default:
-      return {value: undefined, cursor: cursor + size + 1};
-    }
-  }
-
-  function remain(data, cursor) {
-    var d = data.subarray(cursor);
-    return largeArrayToString(d);
-  }
-
-  function _decodeNull(data, cursor) {
-    return {value: null, cursor: cursor + 1};
-  }
-
-  function _decodeBoolean(data, cursor, size) {
-    return {value: (size === 4) ? true : false, cursor: cursor + size + 1};
-  }
-
-  function _decodeInteger(data, cursor, size) {
-    var end = cursor + size;
-    for (var value = 0; cursor < end; cursor++)
-      value = value*10 + (data[cursor] - ZERO);
-
-    return {value: value, cursor: cursor + 1};
-  }
-
-  function _decodeFloat(data, cursor, size) {
-    var value, exp, decimal;
-    var end = cursor + size;
-
-    for(value = 0; data[cursor] !== 46; cursor++)
-      value = value*10 + (data[cursor] - ZERO);
-
-    cursor++;
-    for (decimal = 0, exp=1; cursor < end; cursor++, exp*=10) {
-      decimal = decimal*10 + (data[cursor] - ZERO);
-    }
-
-    return {value: value + (decimal/exp), cursor: cursor + 1};
-  }
-
-  function _decodeString(data, cursor, size) {
-    var d = data.subarray(cursor, cursor + size);
-    var s = largeArrayToString(d);
-    return {value: s, cursor: cursor + size + 1};
-  }
-
-  function _decodeList(data, cursor, size) {
-    if (size === 0)
-      return {value: [], cursor: cursor + 1};
-
-    var list = [];
-    var end  = cursor + size;
-    var result;
-
-    do {
-      result = _decode(data, cursor);
-      list.push(result.value);
-      cursor = result.cursor;
-    } while (cursor < end);
-
-    return {value: list, cursor: result.cursor + 1};
-  }
-
-  function _decodeDict(data, cursor, size) {
-    if (size === 0)
-      return {value: {}, cursor: cursor + 1};
-
-    var dict = {};
-
-    var result = _decodeList(data, cursor, size);
-    var items  = result.value;
-    var len    = items.length;
-    for (var i = 0; i < len; i+=2)
-      dict[items[i]] = items[i + 1];
-
-    return {value: dict, cursor: result.cursor};
-  }
-
-  globalScope.tnetbin = tnetbin;
-}(window));
+}());
 
